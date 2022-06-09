b055b878835a045603b69e8f66c37cac08dc4b5b
Gagan Singh <gagansingh.dev@outlook.com>
2022-06-08 22:27:31 -0400

    Made the serialize interceptor configurable, so it can accept target DTO from the controller instead

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,14 +1,15 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private readonly dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) =>
-        plainToClass(UserDto, data, {
+        plainToInstance(this.dto, data, {
           excludeExtraneousValues: true, // ONLY EXPOSE properties marked @Expose() inside the UserDto
           // Removes all other properties, such as passwords
         }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import {
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 // ROUTE -> /auth/
@@ -20,7 +21,7 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get(`/:id`)
   async findUser(@Param(`id`) id: string) {
     const user = await this.usersService.findOne(+id);
