773c4877c6140577ebad4363218ecb444c95da3a
Gagan Singh <gagansingh.dev@outlook.com>
2022-06-11 23:34:00 -0400

    Implemented the create report route without storing user's details

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private repo: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    // Create the report instance that is to be save in the db
+    const report = this.repo.create(reportDto);
+
+    // Save the new report instance to the db
+    return this.repo.save(report);
+  }
+}
